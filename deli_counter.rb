9f1f044193ae76ba4f318162abca516e7db4e8b7
Christopher Chaison <cchaison0@gmail.com>
2020-03-04 00:00:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(line)
+  if line.length == 0
+    puts "The line is currently empty."
+  else
+    queue = "The line is currently: "
+    line.each_with_index do |name, index|
+      queue << "#{index}. #{name} "
+    end
+  end
+end
